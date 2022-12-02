6c57bd84e6f4e1b57e81f449d87a49c71c5ffb4e
EzeaniChucks <concord_chucks2@yahoo.com>
2022-12-01 20:02:08 -0800

    filled uncompleted pages temporarily

--- pages/energy-req-paed.js
+++ pages/energy-req-paed.js
@@ -1,7 +1,11 @@
-const EnergyPaed =()=>{
-    return (<div>
-        Energy for kids
-    </div>)
-}
-
+import styles from "../components/24hrComp.module.css";
+const EnergyPaed = () => {
+  return (
+    <div className={styles.main}>
+      <h1>Oops!!! Page is in the works.</h1>
+      <img src={"/assets/Calculator_Monochromatic.png"} alt="nameeee" />
+      <h2>I'm working on this page. Should be up in few hours</h2>
+    </div>
+  );
+};
 export default EnergyPaed;
--- pages/estimate-height.js
+++ pages/estimate-height.js
@@ -1,5 +1,12 @@
+import styles from "../components/24hrComp.module.css";
 const EstimateHeight = () => {
-  return <div>Estimate height from length</div>;
+  return (
+    <div className={styles.main}>
+      <h1>Oops!!! Page is in the works.</h1>
+      <img src={"/assets/Calculator_Monochromatic.png"} alt="nameeee" />
+      <h2>I'm working on this page. Should be up in few hours</h2>
+    </div>
+  );
 };
 
 export default EstimateHeight;
