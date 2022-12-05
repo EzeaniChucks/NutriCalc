865ca3bce3546e221e15bd6888bdb1d1ef5ecd2f
EzeaniChucks <concord_chucks2@yahoo.com>
2022-12-05 11:58:06 -0800

    Height estimation validation complete

--- components/estimateHeightComp.js
+++ components/estimateHeightComp.js
@@ -1,12 +1,9 @@
 import styles from "./idw.module.css";
 import styles2 from './energyreq.module.css';
-import { reducer } from "../reducer/IBWReducer";
-import { defaultState } from "../defaultStates/defaultState";
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import HeightForm from "./forms/heightForm";
 
 const EstimateHeightComp = () => {
-    const [state, dispatch] = useReducer(reducer, defaultState);
     
     const [data, setdata] = useState({
       imgsrc: "../assets/ulna.png",
@@ -17,6 +14,8 @@ const EstimateHeightComp = () => {
       ulnalength:0,
       demispan:0,
       kneelength:0,
+      error:'',
+      warning:'',
       result:0
     });
 
@@ -28,8 +27,13 @@ const EstimateHeightComp = () => {
 
     const genResultLength = () => {
         const { age, demispan, ulnalength, kneelength,sex, methodName} = data;
-        console.log(data);
+        if(sex==''|| age==0 || age==''){
+            return setdata({...data, error:`Set gender and age`})
+        }
         if(methodName==='ulna length'){
+            if(ulnalength ==0){
+                return setdata({...data, error:`Ulna length can't be zero`})
+            }
             if (sex==='male'){
                 if(age<65){
                     let result = (79.2 +(3.6*ulnalength)).toFixed(2)
@@ -53,28 +57,44 @@ const EstimateHeightComp = () => {
         }
 
         if(methodName==='demi span'){
+            if(age<16){
+                return setdata({...data, error:`This method isn't suitable for people younger than 16 years. Try the ulna length method`})
+            }
+            if(demispan ==0){
+                return setdata({...data, error:`Demi span can't be zero`})
+            }
             if (sex==='male'){
                 if(age>=16&& age<=54){
                     let result = (68 +(1.3*demispan)).toFixed(2)
-                    setdata({...data, result})
+                    setdata({...data, result, error:''})
                 }
                 if(age>=55){
                     let result = (71 +(1.2*demispan)).toFixed(2)
-                    setdata({...data, result})
+                    setdata({...data, result, error:''})
                 }
             }
             if (sex==='female'){
                 if (age >= 16 && age <= 54) {
                   let result = (62 + 1.3 * demispan).toFixed(2);
-                  setdata({ ...data, result });
+                  setdata({ ...data, result, error:''});
                 }
                 if(age>=55){
                     let result = (67 +(1.2*demispan)).toFixed(2)
-                    setdata({...data, result})
+                    setdata({...data, result, error:''})
                 }
             }
         }
         if(methodName==='knee length'){
+            if (kneelength ==0){
+                return setdata({...data, error:`Knee length can't be zero`})
+            }
+            if (age<18){
+                return setdata({...data, error:`This method isn't suitable for people younger than 18. Try the ulna length method`})
+            }
+            if (age>90){
+                return setdata({...data, error:`This method isn't suitable for people older than 90. Try the ulna length or demiSpan method`})
+            }
+            
             if (sex==='male'){
                 if(age>=18&& age<=60){
                     let result = (71.85 +(1.88*kneelength)).toFixed(2)
@@ -98,13 +118,15 @@ const EstimateHeightComp = () => {
         }
     };
     const switchmethods =(e)=>{
-        const{name}= e.target.dataset
+        const{name}= e.target.dataset;
         if(name==='ulna'){
             setdata({
               ...data,
               imgsrc: "../assets/ulna.png",
               methodName: "ulna length",
               label: "Height from ulna length",
+              result:0,
+              error:''
             });
         }
         if(name==='demi'){
@@ -112,6 +134,8 @@ const EstimateHeightComp = () => {
                 imgsrc:"../assets/demi-span.png",
                 methodName:'demi span',
                 label: "Height from demi span",
+                result:0,
+                error:''
             })
         }
         if(name==='knee'){
@@ -119,9 +143,22 @@ const EstimateHeightComp = () => {
                 imgsrc:"../assets/knee_height_alt.png",
                 methodName:'knee length',
                 label: "Height from knee length",
+                result:0,
+                error:''
             })
         }
     }
+    useEffect(()=>{
+        const methodList = document.querySelectorAll(`.${styles2.kid}`)
+        methodList.forEach((element)=>{
+            return element.classList.remove(`${styles2.active}`);
+        })
+        methodList.forEach(element => {
+            if(element.textContent.toLowerCase() === data.methodName){
+                return element.classList.add(`${styles2.active}`);
+            };
+        });
+    })
     return (
     <div className={styles.main}>
         <div className={styles.container}>
@@ -131,28 +168,39 @@ const EstimateHeightComp = () => {
             <div>
                 <h3>{data.label}</h3>
                 <HeightForm data={data} captureChange={captureChange} genResultLength={genResultLength}/>
-                {data?.result!==0 && <h4>Using {data.methodName} method, the estimated height for this {data?.age}-year-old {data.sex} is <span>{data.result} cm</span> or ({(data.result/100).toFixed(3)} m)</h4>}
+                {data?.error&& <h4 className={styles.error}>{data.error}</h4>}
+                {data?.result!==0 && <h4>Using the {data.methodName} method, the estimated height for {data?.age}-year-old {data.sex}s is <span>{data.result} cm</span> or <span>({(data.result/100).toFixed(3)} m)</span></h4>}
                 <div className={styles2.methodContainer}>
                     <p>Switch methods here</p>
-                    <div id='methodBtns' className={styles2.methodList}>
+                    <div id='heightMethodBtns' className={styles2.methodList}>
                         <p data-name='ulna' onClick={switchmethods} className={styles2.kid}>Ulna length</p>
                         <p data-name='demi' onClick={switchmethods} className={styles2.kid}>Demi Span</p>
-                        <p data-name='knee' onClick={switchmethods} className={styles2.kid}>Knee Height</p>
+                        <p data-name='knee' onClick={switchmethods} className={styles2.kid}>Knee Length</p>
                     </div>
                 </div>
 
                 {data.methodName=='ulna length' && 
                     <>
-                        <h5>To estimate height from ulna length, you'll need a tape measure.</h5>
+                        <h5>You're currently using the <span>{data.methodName}</span> method, appropriate for people of all ages. To estimate height from ulna length, you'll need a tape measure.</h5>
                         <h5>Bend the patient's left arm over their chest (as shown above). Then measure from the
                         the prominent bone on the wrist down to the elbow. Note: It must be the left arm. Not the right.</h5>
                     </>
                 }
                 {data.methodName=='demi span' && 
                     <>
-                        <h5>To estimate height from demispan, you'll need a tape measure.</h5>
-                        <h5>Bend the patient's left arm over their chest (as shown above). Then measure from the
-                        the prominent bone on the wrist down to the elbow. Note: It must be the left arm. Not the right.</h5>
+                        <h5>You're currently using the <span>{data.methodName}</span> method, appropriate for people aged 16 and above. To estimate height from demispan, you'll need a tape measure.</h5>
+                        <h5>The patient may sit on a chair or lie on their backs. Stretch the right arm as shown in the diagram above.
+                            The tape is placed between the middle and ring finger and runs smoothly along the arm. The mesurement is taken from between the fingers
+                            to the sternal notch above the chest.
+                        </h5>
+                    </>
+                }
+                {data.methodName=='knee length' && 
+                    <>
+                        <h5>You're currently using the <span>{data.methodName}</span> method, appropriate for people aged 18 to 90. To estimate height from demispan, you'll need a tape measure.</h5>
+                        <h5>To measure knee height, the knee should be bent at 90 degrees to the ankle (as shown above).
+                            Measure from the top of the thighs to the base of the feet (as if the feet were placed flat on the floor)
+                        </h5>
                     </>
                 }
             </div>      
